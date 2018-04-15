3a73380d2161c9812ab300fd59b3e813a7b5cc69
Dow Drake <dowdrake@msn.com>
2018-04-14 20:48:12 -0700

    fully automated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # mines
 ## Analyze Minesweeper Scenarios
 
-I find Minesweeper interesting because it combines pure logic with probability.  This script is designed to help improve intuition on the probability side by making it possible to fairly quickly compute the set of solutions and sum them up to get an idea of the relative likelihood that a mine will be present in a given square.
+I find Minesweeper interesting because it combines pure logic with probability.  This script is designed to help improve intuition on the probability side by making it possible to fairly quickly compute the set of solutions and sum them up to get an idea of the relative likelihood that a mine will be present in a given square.  The current version of the code doesn't handle explicit mines, but you can treat them as possible mines.
--- mines.py
+++ mines.py
@@ -11,7 +11,6 @@ class Qnode:
         self.nnodes = []
 
     def set(self, state):
-        #print("setting qnode ", self.name, state)
         self.state = state
         if self.state == True:
             for n in self.nnodes:
@@ -58,14 +57,12 @@ class Nnode:
 def step(qname):
     for n in nnodes.values():
         if n.numleft() > 0 and n .possct() == 0:
-            print("contradiction")
             return None
 
     minval = sys.maxsize
     maxval = -1
     for n in nnodes.values():
         if n.possct() == 1:
-            #print(n.possibles[0].name)
             return n
         if n.possct() != 0 and n.possct() < minval:
             minval = n.possct()
@@ -80,38 +77,52 @@ def step(qname):
 # given a list of qnode names, pop the last, then try to compute a solution
 # add it to the set and append it to the output file.  
 # If we arrive at a branch point.  Pop the next 
-def solve(qname):
+def solve():
     global solutions
+    global seq_queue
+    curlist = seq_queue.pop()
+    curidx = 0
     done = False
     while not done:
+        qname = curlist[curidx]
+        curidx += 1
         qnodes[qname].set(True)
+        # take a step
         n = step(qname)
         if n == None:
-            print("found contradiction, quitting")
+            if debug: 
+                print("found contradiction with {}, quitting".format(qname))
+                print(curlist)
             done = True
         else:
             if n == True:
                 mines = [q.name for q in qnodes.values() if q.state == True]
-                print("found solution: {}".format(mines))
+                if debug: 
+                    print("found solution: {}".format(mines))
+                    print(curlist)
                 solutions.add(tuple(sorted(mines)))
                 done = True
-            elif n != None:
-                #print("found node {0} with numleft = {1:d} and possct = {2:d}" \
-                #    .format(n.name, n.numleft(), n.possct()))
+            elif curidx >= len(curlist) and n != None:
                 if n.possct() == 1:
                     qname = n.possibles[0].name
+                    if debug: 
+                        print("only one possible cell to check: {}".format(qname))
+                        print(curlist)
+                    curlist.append(qname)
                 else:
-                    possqs = [q.name for q in n.possibles]
-                    print("step returned node {0} with possibles:".format(n.name))
-                    print(possqs)
-                    valid = False
-                    while not valid:
-                        print("enter one to try or 0 to quit")
-                        qname = input(">>> ")
-                        if qname == '0':
-                            exit(0)
-                        if qname in possqs:
-                            valid = True
+                    if debug: 
+                        print("several possible cells to check")
+                    for q in n.possibles:
+                        newlist = curlist[:]
+                        newlist.append(q.name)
+                        if debug: 
+                            print("appending new list to queue", newlist)
+                        seq_queue.append(newlist)
+                    curlist = seq_queue.pop()
+            else:
+                if debug: 
+                    print("testing the next in the current list")
+                    print(curlist)
 
 def write_out():
     text = "\n".join([str(sol) for sol in sorted(list(solutions))]) + "\n"
@@ -131,6 +142,7 @@ def reset():
     qnodes = {name: Qnode(name) for name in lines[0].split(",")}
     for i in range(1,len(lines)):
         name, count, possibles = lines[i].split(",")
+        possibles = possibles[1:-1].split()
         nnodes[name] = Nnode(name,int(count),possibles,qnodes)
 
 if len(sys.argv) != 2:
@@ -140,26 +152,17 @@ if len(sys.argv) != 2:
 with open(sys.argv[1],'r') as f:
     text = f.read()
 
+debug = True
 solutions = set()
 qnodes = {}
 nnodes = {}
 reset()
-qname = '1'
-while qname != '0':
+seq_queue = [[q] for q in qnodes.keys()]
+while len(seq_queue) > 0:
+    solve()
     print("{0:d} solution(s) found.\n".format(len(solutions)))
-    valid = False
-    while not valid:
-        print("qnodes are: {0}".format(", ".join(list(qnodes.keys()))))
-        print("enter one to try or 0 to quit")
-        qname = input(">>> ")
-        if qname == '0':
-            write_out()
-            exit(0)
-        if qname in qnodes.keys():
-            valid = True
     write_out()
     reset()
-    solve(qname)
 
 # input1.txt represents the following state
 # where cells a-h are nnodes (cells with number values)
